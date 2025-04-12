f5586403b82ab9a9dcdcdd3e4a58cbcb7f234197
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-04-12 19:40:16 +0200

    BREAKING CHANGE: moved from react-router to tanstack router for frontend routing
    Moved the frontend to TanStack router!

---
+++ frontend/src/stores/auth.store.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import type { AuthStore } from "@/types/stores/auth";
+
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (get, set) => ({
+      user: null,
+      accessToken: null,
+      accessTokenExpiresAt: null,
+      refreshToken: null,
+      refreshTokenExpiresAt: null,
+    }),
+    { name: "auth-storage" },
+  ),
+);
