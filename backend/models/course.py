2c014e0cf78b22e8d606f60e4bc54ff73f08c339
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-04-16 16:11:30 +0200

    feat: courses table migration

---
+++ backend/models/course.py
@@ -0,0 +1,20 @@
+from sqlalchemy import UUID, Column, DateTime, ForeignKey, String, func, null
+from sqlalchemy.orm import relationship
+from database.base import Base
+
+
+class Course(Base):
+    __tablename__ = "courses"
+
+    id = Column(UUID, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+
+    user_id = Column(UUID, ForeignKey("users.id"))
+    user = relationship("User", back_populates="courses")
+
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import UUID, Column, DateTime, Integer, String, func, null
+from sqlalchemy import UUID, Column, DateTime, Integer, String, func
+
+from sqlalchemy.orm import relationship
 from database.base import Base
 
 
@@ -10,5 +12,9 @@ class User(Base):
     last_name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
+
+    courses = relationship("Course", back_populates="user")
