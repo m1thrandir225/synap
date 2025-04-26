aee70f8d5650f484ace9d779802c2c83fcfa4d02
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-04-26 20:21:14 +0200

    feat: UserMenu includes proper user information

--- frontend/src/components/UserMenu.tsx
+++ frontend/src/components/UserMenu.tsx
@@ -15,13 +15,15 @@ import {
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import { ChevronsUpDown, LogOut } from "lucide-react";
 import { useAuthStore } from "@/stores/auth.store";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import { redirect, useLocation, useRouter } from "@tanstack/react-router";
+import { extractInitials } from "@/lib/utils";
 const UserMenu: React.FC = () => {
   const { isMobile } = useSidebar();
-  const { logout } = useAuthStore();
+  const { logout, user } = useAuthStore();
   const location = useLocation();
   const router = useRouter();
+
   const logoutWithRedirect = useCallback(() => {
     logout();
     router.navigate({
@@ -32,6 +34,11 @@ const UserMenu: React.FC = () => {
       replace: true,
     });
   }, [logout]);
+
+  const initials = useMemo(() => {
+    if (!user) return "";
+    return extractInitials(user.first_name, user.last_name);
+  }, [user]);
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -39,11 +46,15 @@ const UserMenu: React.FC = () => {
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton size="lg">
               <Avatar>
-                <AvatarFallback className="rounded-lg">JD</AvatarFallback>
+                <AvatarFallback className="rounded-lg">
+                  {initials}
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">James Doe</span>
-                <span className="truncate text-xs">james.doe@gmail.com</span>
+                <span className="truncate font-semibold">
+                  {user?.first_name} {user?.last_name}
+                </span>
+                <span className="truncate text-xs">{user?.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -57,11 +68,15 @@ const UserMenu: React.FC = () => {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarFallback className="rounded-lg">JD</AvatarFallback>
+                  <AvatarFallback className="rounded-lg">
+                    {initials}
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">James Doe</span>
-                  <span className="truncate text-xs">james.doe@gmail.com</span>
+                  <span className="truncate font-semibold">
+                    {user?.first_name} {user?.last_name}
+                  </span>
+                  <span className="truncate text-xs">{user?.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function extractInitials(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)} ${lastName.charAt(0)}`.toUpperCase();
 }
