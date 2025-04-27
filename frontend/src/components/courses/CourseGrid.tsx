2c0b945f8fb0f10d43f7b11e84e253ae5bb3c342
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-04-27 16:27:26 +0200

    feat: single course view

--- frontend/src/components/courses/CourseGrid.tsx
+++ frontend/src/components/courses/CourseGrid.tsx
@@ -9,7 +9,7 @@ const CourseGrid: React.FC<ComponentProps> = ({ items, ...props }) => {
   return (
     <div className="w-full gap-12 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
       {items.map((course) => (
-        <CourseCard course={course} key={course.id} />
+        <CourseCard inGrid course={course} key={course.id} />
       ))}
     </div>
   );
--- frontend/src/routes/dashboard/courses/$courseId.tsx
+++ frontend/src/routes/dashboard/courses/$courseId.tsx
@@ -1,9 +1,26 @@
-import { createFileRoute } from '@tanstack/react-router'
+import CourseCard from "@/components/courses/CourseCard";
+import CourseLectures from "@/components/courses/CourseLectures";
+import CourseNotes from "@/components/courses/CourseNotes";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/dashboard/courses/$courseId')({
+export const Route = createFileRoute("/dashboard/courses/$courseId")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/courses/$courseId"!</div>
+  return (
+    <div className="w-full h-full flex flex-col items-start gap-8">
+      <CourseCard
+        course={{
+          id: "",
+          name: "Example",
+          content: "Example Description",
+        }}
+      />
+
+      <CourseLectures items={[]} />
+
+      <CourseNotes items={[]} />
+    </div>
+  );
 }
