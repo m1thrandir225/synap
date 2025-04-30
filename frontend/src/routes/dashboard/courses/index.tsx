3bd46e8f2d28fafb24892537b869ef32a8ac40f1
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-04-30 21:22:26 +0200

    fix: styling on courses  index page like the other pages

--- frontend/src/routes/dashboard/courses/index.tsx
+++ frontend/src/routes/dashboard/courses/index.tsx
@@ -3,6 +3,7 @@ import { dummyCourses } from "@/types/models/course";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { Plus } from "lucide-react";
 import CourseGrid from "@/components/courses/CourseGrid";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 export const Route = createFileRoute("/dashboard/courses/")({
   loader: () => {
     return {
@@ -16,15 +17,24 @@ function RouteComponent() {
   const courses = dummyCourses;
   return (
     <div className="w-full h-full flex flex-col items-start gap-8">
-      <div className="flex flex-row items-center justify-between w-full">
-        <h1 className="font-bold text-2xl">Courses</h1>
-        <Button asChild size={"icon"} variant={"default"}>
-          <Link to="/dashboard/courses/new">
-            <Plus />
-          </Link>
-        </Button>
-      </div>
-      <CourseGrid items={courses} />
+      <Card className="w-full">
+        <CardHeader>
+          <div className="flex flex-row items-center justify-between w-full">
+            <CardTitle>Courses</CardTitle>
+            <Button asChild size={"icon"} variant={"default"}>
+              <Link to="/dashboard/courses/new">
+                <Plus />
+              </Link>
+            </Button>
+          </div>
+        </CardHeader>
+      </Card>
+
+      <Card className="w-full">
+        <CardContent>
+          <CourseGrid items={courses} />
+        </CardContent>
+      </Card>
     </div>
   );
 }
