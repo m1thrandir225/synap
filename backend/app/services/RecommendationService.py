d41bef69b81696194ee877e5caf013cbd99dd69c
FilipKonzales123 <filip.projchevski@students.finki.ukim.mk>
2025-05-03 10:43:08 +0200

    Update RecommendationService.py

--- backend/app/services/RecommendationService.py
+++ backend/app/services/RecommendationService.py
@@ -2,13 +2,14 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 from ..repositories.recommendation_repository import RecommendationRepository
 from ..database.models.recommendation import Recommendation
+from models import CreateRecommendationDTO, UpdateRecommendationDTO
 
 class RecommendationService:
     def __init__(self, recom_repo: RecommendationRepository):
         self.recommendation_repository = recom_repo
 
-    def create_recommendation(self, file_id: UUID, learning_material_id: UUID, relevance_score: float) -> Recommendation:
-        return self.recommendation_repository.create_recommendation(file_id, learning_material_id, relevance_score)
+    def create_recommendation(self, recom_data: CreateRecommendationDTO) -> Recommendation:
+        return self.recommendation_repository.create_recommendation(recom_data.dict())
 
     def get_recommendation(self, recommendation_id: UUID) -> Recommendation | None:
         return self.recommendation_repository.get_recommendation_by_id(recommendation_id)
@@ -19,8 +20,8 @@ class RecommendationService:
     def get_recommendations_for_learning_material(self, learning_material_id: UUID) -> list[Recommendation]:
         return self.recommendation_repository.get_recommendations_by_learning_material_id(learning_material_id)
 
-    def update_relevance_score(self, recommendation_id: UUID, relevance_score: float) -> Recommendation | None:
-        return self.recommendation_repository.update_recommendation_relevance(recommendation_id, relevance_score)
+    def update_relevance_score(self, recommendation_id: UUID, recom_data: UpdateRecommendationDTO) -> Recommendation | None:
+        return self.recommendation_repository.update_recommendation_relevance(recommendation_id, recom_data.dict(exclude_unset=True))
 
     def delete_recommendation(self, recommendation_id: UUID) -> bool:
         return self.recommendation_repository.delete_recommendation(recommendation_id)
