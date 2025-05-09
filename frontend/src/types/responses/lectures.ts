868eee28460278e9eedb6546450d15a20f1fbeec
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-05-09 21:04:46 +0200

    feat: lecture CRUD(WIP)

---
+++ frontend/src/types/responses/lectures.ts
@@ -0,0 +1,9 @@
+import type { Lecture } from "../models/lecture";
+
+export type CreateLectureRequest = {};
+
+export type CreateLectureResponse = Lecture & {};
+
+export type EditLectureRequest = {};
+
+export type EditLectureResponse = {};
