fb7f0d41c3fee1d819dbfa5c4b7821bcb8334883
FilipKonzales123 <filip.projchevski@students.finki.ukim.mk>
2025-05-16 16:08:01 +0200

    Update summarization_router.py

--- backend/app/routers/summarization_router.py
+++ backend/app/routers/summarization_router.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter, HTTPException, status
 from fastapi.params import Depends
 from uuid import UUID
 from app.dependencies import  get_uploaded_files_service
-from app.services.summarization_service import SummarizationService, get_summarization_service
-from app.services.uploaded_files_service import UploadedFileService
-from app.models.summarization import CreateSummarization
+from app.services import SummarizationService, get_summarization_service
+from app.services import UploadedFileService
+from app.models CreateSummarization
 
 
 router = APIRouter(prefix="/summarization", tags=["summarization_router"])
@@ -15,9 +15,9 @@ router = APIRouter(prefix="/summarization", tags=["summarization_router"])
 async def summarize_file(
     summarization: CreateSummarization,
     summarization_service: Annotated[SummarizationService, Depends(get_summarization_service)],
-    uploaded_files_repo: Annotated[UploadedFileService, Depends(get_uploaded_files_service)], 
+    uploaded_files_service: Annotated[UploadedFileService, Depends(get_uploaded_files_service)], 
 ):
-    uploaded_file = uploaded_files_repo.get_uploaded_file(summarization.file_id)
+    uploaded_file = uploaded_files_service.get_uploaded_file(summarization.file_id)
     
     if not uploaded_file:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File not found")
